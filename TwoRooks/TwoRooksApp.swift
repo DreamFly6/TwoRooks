3a658e8e7b06d864024aad38e5c787129cc560fc
Samuel McBroom <sam@sammcb.com>
2021-02-10 10:59:31 -0800

    Initial Commit

---
+++ TwoRooks/TwoRooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoRooksApp.swift
+//  TwoRooks
+//
+//  Created by Samuel McBroom on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TwoRooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
